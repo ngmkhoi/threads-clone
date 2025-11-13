fff26590915b8997a6f96d712c37ebac590e7720
Nguyen Minh Khoi <tpfkhoi0411@gmail.com>
2025-11-13 20:05:38 +0700

    add dialog messages for user interactions and update styles for better UX

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
     plugins: [react({
         babel: {
             plugins: [['babel-plugin-react-compiler', { runtime: 'automatic' }]]
         }
     })],
-    base: '/',
+    base: command === 'serve' ? '/' : '/threads-clone/',
     resolve: {
         alias: {
             //eslint-disable-next-line no-undef
             "@": path.resolve(__dirname, "./src"),
         },
     }
-});
+}));
