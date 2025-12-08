74549bda9bec9f95c3fc3da7ec5263503b2c62c0
Nguyen Minh Khoi <tpfkhoi0411@gmail.com>
2025-12-09 01:00:49 +0700

    feat: prevent dialog event propagation.

--- src/components/ui/dialog.jsx
+++ src/components/ui/dialog.jsx
@@ -15,6 +15,8 @@ const DialogClose = DialogPrimitive.Close
 const DialogOverlay = React.forwardRef(({ className, ...props }, ref) => (
   <DialogPrimitive.Overlay
     ref={ref}
+    onClick={(e) => e.stopPropagation()}
+    onInteractOutside={(e) => e.stopPropagation()}
     className={cn(
       "fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
       className
@@ -30,6 +32,8 @@ const DialogContent = React.forwardRef(({ className, children, ...props }, ref) 
     <DialogOverlay />
     <DialogPrimitive.Content
       ref={ref}
+      onPointerDownOutside={(e) => e.stopPropagation()}
+      onInteractOutside={(e) => e.stopPropagation()}
       className={cn(
         "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 sm:rounded-lg",
         className
